a10fd4d097ec829cb7687e50d69a6c625d638511
Jaylon Slebo <jaylon.slebo.2023@student.lhschools.org>
2020-02-20 11:03:23 -0500

    Asked for the input for f_p

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -34,6 +34,9 @@ Scanner Keyboard;
 Keyboard = new Scanner(System.in);
 System.out.println("How many stars on average are formed per year?");
 rStar = Keyboard.nextInt();
+System.out.println("How many stars have planets around them?");
+f_p = Keyboard.nextDouble();
+
 N = rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L;
 
 System.out.println("The number of inteligent species that are out there\n" +
